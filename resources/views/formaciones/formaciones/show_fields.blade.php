5d5b047b4529fbc07c9cd91e97ca86b1992b9cad
Valentina Londoño Marin <valoma.vld@gmail.com>
2020-11-29 22:43:18 -0500

    Actualizaciones en vista con atributos foraneos para las formaciones

--- resources/views/formaciones/formaciones/show_fields.blade.php
+++ resources/views/formaciones/formaciones/show_fields.blade.php
@@ -7,19 +7,19 @@
 <!-- Entidad Id Field -->
 <div class="form-group">
     {!! Form::label('entidad_id', __('models/formaciones.fields.entidad_id').':') !!}
-    <p>{{ $formacion->entidad_id }}</p>
+    <p>{{ $formacion->entidad->nombre }}</p>
 </div>
 
 <!-- Nivel Formacion Id Field -->
 <div class="form-group">
     {!! Form::label('nivel_formacion_id', __('models/formaciones.fields.nivel_formacion_id').':') !!}
-    <p>{{ $formacion->nivel_formacion_id }}</p>
+    <p>{{ $formacion->nivelFormacion->nombre }}</p>
 </div>
 
 <!-- Area Conocimiento Id Field -->
 <div class="form-group">
     {!! Form::label('campo_educacion_id', __('models/formaciones.fields.campo_educacion_id').':') !!}
-    <p>{{ $formacion->campo_educacion_id }}</p>
+    <p>{{ $formacion->campoEducacion->nombre }}</p>
 </div>
 
 <!-- Activo Field -->
