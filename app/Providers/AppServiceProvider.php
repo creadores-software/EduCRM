59b798f96978b4fd38815aa3655054b2ad8f32bc
Valentina Londoño Marin <valoma.vld@gmail.com>
2020-12-04 22:45:04 -0500

    Se añade validación de tipo unique no sensible a mayúsculas y minúsculas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('iunique', function ($attribute, $value, $parameters, $validator) {
+            $query = DB::table($parameters[0]);
+            $column = $query->getGrammar()->wrap($parameters[1]);
+    
+            if (isset($parameters[2])) {
+                if (isset($parameters[3])) {
+                    $idCol = $parameters[3];
+                } else {
+                    $idCol = 'id';
+                }
+    
+                $query->where($idCol, '!=', $parameters[2]);
+            }
+    
+            return ! $query->whereRaw("lower({$column}) = lower(?)", [$value])->count();
+        });    
     }
 }
