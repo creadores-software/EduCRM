ac4e8c662f596ed9ed60dc5ac788400c9af18571
Valentina Londoño Marin <valoma.vld@gmail.com>
2020-12-10 22:42:12 -0500

    Se añade pestaña preferencias

--- resources/views/contactos/contactos/nav_show.blade.php
+++ resources/views/contactos/contactos/nav_show.blade.php
@@ -2,7 +2,9 @@
     <li class="{{ (request()->is('contactos/contactos*')) ? 'active' : '' }}">
         <a href="{{ route('contactos.contactos.show',$idContacto) }}"><i class="fa fa-user"></i> General</a></li>
     <li class="{{ (request()->is('contactos/informacionesRelacionales*')) ? 'active' : '' }}">
-        <a href="{{ route('contactos.informacionesRelacionales.show',$idRelacional) }}"><i class="fa fa-thumbs-up"></i> Relacional</a></li>
+        <a href="{{ route('contactos.informacionesRelacionales.show',$idRelacional) }}"><i class="fa fa-heart"></i> Relacional</a></li>
+    <li class="{{ (request()->is('contactos/preferencia*')) ? 'active' : '' }}">
+        <a href="{{ route('contactos.preferenciasFormaciones.index',['idContacto'=>$idContacto,'soloVista'=>true]) }}"><i class="fa fa-thumbs-up"></i> Preferencias</a></li>
     <li class="{{ (request()->is('contactos/informacionesAcademicas*')) ? 'active' : '' }}">
         <a href="{{ route('contactos.informacionesAcademicas.index',['idContacto'=>$idContacto,'soloVista'=>true]) }}"><i class="fa fa-graduation-cap"></i> Académica</a></li>
     <li class="{{ (request()->is('contactos/informacionesLaborales*')) ? 'active' : '' }}">
