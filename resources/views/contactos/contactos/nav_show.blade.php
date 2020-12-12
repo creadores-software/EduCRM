355615d75f3e97b60df18faabe941017a1cc34ea
Valentina Londoño Marin <valoma.vld@gmail.com>
2020-12-11 22:19:20 -0500

    Se eliminan las vistas inidividuales de las preferencias al igual que los datatable pues serán gestionados directamente desde la información relacional

--- resources/views/contactos/contactos/nav_show.blade.php
+++ resources/views/contactos/contactos/nav_show.blade.php
@@ -3,8 +3,6 @@
         <a href="{{ route('contactos.contactos.show',$idContacto) }}"><i class="fa fa-user"></i> General</a></li>
     <li class="{{ (request()->is('contactos/informacionesRelacionales*')) ? 'active' : '' }}">
         <a href="{{ route('contactos.informacionesRelacionales.show',$idRelacional) }}"><i class="fa fa-heart"></i> Relacional</a></li>
-    <li class="{{ (request()->is('contactos/preferencia*')) ? 'active' : '' }}">
-        <a href="{{ route('contactos.preferencias.show',['idContacto'=>$idContacto]) }}"><i class="fa fa-thumbs-up"></i> Preferencias</a></li>
     <li class="{{ (request()->is('contactos/informacionesAcademicas*')) ? 'active' : '' }}">
         <a href="{{ route('contactos.informacionesAcademicas.index',['idContacto'=>$idContacto,'soloVista'=>true]) }}"><i class="fa fa-graduation-cap"></i> Académica</a></li>
     <li class="{{ (request()->is('contactos/informacionesLaborales*')) ? 'active' : '' }}">
