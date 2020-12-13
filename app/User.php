23e0adef2e04308e4ed6077810788aa64cc4c0a8
Valentina Londoño Marin <valoma.vld@gmail.com>
2020-12-12 23:41:42 -0500

    Identificación del usuario para el nuevo paquete de auditoria

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Altek\Accountant\Contracts\Identifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Identifiable
 {
     use Notifiable;
 
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getIdentifier()
+    {
+        return $this->getKey();
+    }
 }
