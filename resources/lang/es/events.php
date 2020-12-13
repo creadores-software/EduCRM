b3fddde56edc528d29fe4b08a3d38a547bde42e0
Valentina Londoño Marin <valoma.vld@gmail.com>
2020-12-12 23:41:00 -0500

    Se añade y configura el paquete accountant y eventually de altek para el manejo de auditoria

--- resources/lang/es/events.php
+++ resources/lang/es/events.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
-
     'created'      => 'Crear',
     'updated'      => 'Actualizar',
     'deleted'      => 'Eliminar',
     'restored'     => 'Restaurar',
+    'forceDeleted' => 'Eliminar',
+    'retrieved'    => 'Obtenido',
+    'toggled'      => 'Actualizar',
+    'synced'       => 'Actualizar',
+    'existingPivotUpdated'      => 'Actualizar',
+    'attached'     => 'Actualizar',
+    'detached'     => 'Actualizar',
 ];
