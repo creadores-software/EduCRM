2f59c43d77eb0b5e06ff2f36b628836f310f04a4
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-04-18 18:02:21 -0500

    Actualización en vista para limitar botones según permisos.

--- resources/views/contactos/informaciones_universitarias/datatables_actions.blade.php
+++ resources/views/contactos/informaciones_universitarias/datatables_actions.blade.php
@@ -4,13 +4,17 @@
     <a href="{{ route('contactos.informacionesUniversitarias.show', [$id,'idContacto'=>$idContacto]) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-eye-open"></i>
     </a>
+    @can('contactos.informacionesUniversitarias.editar')
     <a href="{{ route('contactos.informacionesUniversitarias.edit', [$id,'idContacto'=>$idContacto]) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-edit"></i>
     </a>
+    @endcan
+    @can('contactos.informacionesUniversitarias.eliminar')
     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
         'type' => 'submit',
         'class' => 'btn btn-danger btn-xs',
         'onclick' => 'return confirm("'.__('crud.are_you_sure').'")'
     ]) !!}
+    @endcan
 </div>
 {!! Form::close() !!}
--- resources/views/formaciones/niveles_formacion/datatables_actions.blade.php
+++ resources/views/formaciones/niveles_formacion/datatables_actions.blade.php
@@ -3,13 +3,17 @@
     <a href="{{ route('formaciones.nivelesFormacion.show', $id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-eye-open"></i>
     </a>
+    @can('formaciones.nivelesFormacion.editar')
     <a href="{{ route('formaciones.nivelesFormacion.edit', $id) }}" class='btn btn-default btn-xs'>
         <i class="glyphicon glyphicon-edit"></i>
     </a>
+    @endcan
+    @can('formaciones.nivelesFormacion.eliminar')
     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', [
         'type' => 'submit',
         'class' => 'btn btn-danger btn-xs',
         'onclick' => 'return confirm("'.__('crud.are_you_sure').'")'
     ]) !!}
+    @endcan
 </div>
 {!! Form::close() !!}
