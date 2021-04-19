c90ae41a25f18c0bccb74f7a23e908d1b8a362f4
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-04-18 19:41:04 -0500

    Actualización de la interfaz de error

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="content">  
     <div class="alert alert-danger">        
-        <h4><i class="icon fa fa-ban"></i> Se ha generado una excepción</h4>
         <p>{{$message}}</p>
         <p>Si tienes inquietudes o el error persiste, contacta al administrador.</p>
     </div>
