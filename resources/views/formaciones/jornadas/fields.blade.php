669938641ea2af836ea2394de9dc32b00b21f36b
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-05-01 11:32:58 -0500

    Se dejan como únicos los nombres de jornadas y sisbenes

--- resources/views/formaciones/jornadas/fields.blade.php
+++ resources/views/formaciones/jornadas/fields.blade.php
@@ -2,6 +2,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('nombre', __('models/jornadas.fields.nombre').':') !!}
     {!! Form::text('nombre', null, ['class' => 'form-control']) !!}
+    {!! Form::hidden('id', old('id', $jornada->id ?? '')) !!}
 </div>
 
 <!-- Submit Field -->
--- resources/views/parametros/sisbenes/fields.blade.php
+++ resources/views/parametros/sisbenes/fields.blade.php
@@ -2,6 +2,7 @@
 <div class="form-group col-sm-6">
     {!! Form::label('nombre', __('models/sisbenes.fields.nombre').':') !!}
     {!! Form::text('nombre', null, ['class' => 'form-control']) !!}
+    {!! Form::hidden('id', old('id', $sisben->id ?? '')) !!}
 </div>
 
 <!-- Submit Field -->
