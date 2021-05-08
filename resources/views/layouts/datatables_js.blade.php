29b98337fcc60d6621219a1c49b2ef5b797ad676
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-05-08 14:37:56 -0500

    Se traen de manera local los script y estilos de datatable

--- resources/views/layouts/datatables_js.blade.php
+++ resources/views/layouts/datatables_js.blade.php
@@ -1,8 +1,8 @@
 <!-- Datatables -->
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.colVis.min.js"></script>
-<script src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js"></script>
+<script src="/js/datatable/jquery.dataTables.min.js"></script>
+<script src="/js/datatable/dataTables.bootstrap.min.js"></script>
+<script src="/js/datatable/dataTables.buttons.min.js"></script>
+<script src="/js/datatable/buttons.bootstrap.min.js"></script>
+<script src="/js/datatable/buttons.colVis.min.js"></script>
+<script src="/js/datatable/dataTables.select.min.js"></script>
 <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
