4f9f4bf6b270c37c752eebd9bda36d759fcad65e
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-05-14 11:59:33 -0500

    Actualización de fecha de última interaccion en la oportunidad

--- app/Repositories/Campanias/InteraccionRepository.php
+++ app/Repositories/Campanias/InteraccionRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Campanias;
 
 use App\Models\Campanias\Interaccion;
 use App\Repositories\BaseRepository;
+use Carbon\Carbon;
 
 /**
  * Class InteraccionRepository
@@ -43,5 +44,40 @@ class InteraccionRepository extends BaseRepository
     public function model()
     {
         return Interaccion::class;
+    }
+
+    /**
+     * Create model record
+     * @param array $input
+     * @return Model
+     */
+    public function create($input)
+    {
+        $model = $this->model->newInstance($input);
+        $model->save();
+        $model->oportunidad->ultima_interaccion= new Carbon(); 
+        $model->oportunidad->save();
+        return $model;
+    }
+
+    /**
+     * Update model record for given id
+     *
+     * @param array $input
+     * @param int $id
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|Model
+     */
+    public function update($input, $id)
+    {
+        $query = $this->model->newQuery();
+        $model = $query->findOrFail($id);
+        $model->fill($input);
+        $model->save();
+        if ($model->wasChanged()) {
+            $model->oportunidad->ultima_interaccion= new Carbon(); 
+            $model->oportunidad->save();
+        }
+        return $model;
     }
 }
