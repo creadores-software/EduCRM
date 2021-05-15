56df24fa4ffd98235100143b576ee62befdb0717
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-05-15 16:09:58 -0500

    Se da formato a la vista de segmentos añadiendo vista previa.

--- resources/views/contactos/segmentos/show.blade.php
+++ resources/views/contactos/segmentos/show.blade.php
@@ -9,8 +9,11 @@
     <div class="content">
         <div class="box box-primary">
             <div class="box-body">
-                <div class="row" style="padding-left: 20px">
-                    <h2 class="page-header">Datos</h2>
+                <div class="row col-md-12" style="padding-left: 20px">
+                    <h2 class="pull-right">
+                        <a class="btn btn-primary" target="_blank" href="{{ route('contactos.contactos.index',['vistaPrevia'=>1,'idSegmento'=>$segmento->id]) }}">Vista previa</a> 
+                    </h2> 
+                    <h2 class="page-header">Datos</h2>                    
                     @include('contactos.segmentos.show_fields')
                     @include('layouts.audit')
                     <a href="{{ route('contactos.segmentos.index') }}" class="btn btn-default">
--- resources/views/contactos/segmentos/show_fields.blade.php
+++ resources/views/contactos/segmentos/show_fields.blade.php
@@ -1,34 +1,34 @@
 <!-- Nombre Field -->
-<div class="form-group">
+<div class="form-group col-sm-6">
     {!! Form::label('nombre', __('models/segmentos.fields.nombre').':') !!}
     <p>{{ $segmento->nombre }}</p>
 </div>
 
 <!-- Descripcion Field -->
-<div class="form-group">
+<div class="form-group col-sm-6">
     {!! Form::label('descripcion', __('models/segmentos.fields.descripcion').':') !!}
     <p>{{ $segmento->descripcion }}</p>
 </div>
 
+<!-- Global Field -->
+<div class="form-group col-sm-6" >
+    {!! Form::label('global', __('models/segmentos.fields.global').':') !!}
+    <p>{{ $segmento->global? 'Si' : 'No' }}</p>
+</div>
+
+<!-- Usuario Id Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('usuario_id', __('models/segmentos.fields.usuario_id').':') !!}
+    <p>{{ $segmento->usuario->name }}</p>
+</div>
+
 <!-- Filtros Field -->
-<div class="form-group">
+<div class="form-group col-sm-12">
     {!! Form::label('filtros', __('models/segmentos.fields.filtros').':') !!}
     <ul>
         @foreach ($segmento->filtros as $filtro)
             <li><b>{{ $filtro['campo'] }}</b>: {{ $filtro['valor'] }}</li>
         @endforeach
     </ul>
-</div>
-
-<!-- Global Field -->
-<div class="form-group" >
-    {!! Form::label('global', __('models/segmentos.fields.global').':') !!}
-    <p>{{ $segmento->global? 'Si' : 'No' }}</p>
-</div>
-
-<!-- Usuario Id Field -->
-<div class="form-group">
-    {!! Form::label('usuario_id', __('models/segmentos.fields.usuario_id').':') !!}
-    <p>{{ $segmento->usuario->name }}</p>
 </div>
 
