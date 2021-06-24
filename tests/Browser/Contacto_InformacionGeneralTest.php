f6cf2ffabfe4f66cf46ee805e3f2463ba2634032
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-06-24 00:18:28 -0500

    Se adicionan nuevas pruebas al test dusk de contactos

--- tests/Browser/Contacto_InformacionGeneralTest.php
+++ tests/Browser/Contacto_InformacionGeneralTest.php
@@ -3,10 +3,14 @@
 namespace Tests\Browser;
 
 use App\Models\Admin\User;
+use App\Models\Contactos\Contacto;
 use App\Models\Parametros\Origen;
+use App\Models\Parametros\Prefijo;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+use function Symfony\Component\String\b;
 
 class Contacto_InformacionGeneralTest extends DuskTestCase
 {  
@@ -30,7 +34,7 @@ class Contacto_InformacionGeneralTest extends DuskTestCase
 
     }  
 
-     /**
+    /**
      * Valida que muestre el contacto a seleccionar con la opción referido
      */
     public function testAsociarReferido()
@@ -41,9 +45,57 @@ class Contacto_InformacionGeneralTest extends DuskTestCase
                 $browser->loginAs(User::find(1));//Superadmin
                 $browser->visit('/contactos/contactos/create');
                 $browser->assertDontSee('Referido Por');
-                $browser->script('jQuery("#origen_id").append($("<option selected=\'selected\'></option>").val('.$referido->id.').text("Referido")).trigger("change");');
+                $browser->asignarValorSelect2('#origen_id','Referido',$referido->id);
                 $browser->assertSee('Referido Por');
             });
         }
-    }  
+    } 
+    
+    /**
+     * Valida que no permita crear un contacto con la misma cédula (repetido)
+     */
+    public function testContactoRepetido()
+    {
+        $contacto = factory(Contacto::class)->create();
+        $this->browse(function (Browser $browser) use ($contacto){
+            $browser->loginAs(User::find(1));//Superadmin
+            $browser->visit('/contactos/contactos/create');
+            $browser->type('identificacion', $contacto->identificacion); 
+            $browser->press('Guardar');             
+            $browser->assertSee('No se debe repetir');
+        }); 
+        $contacto->delete();
+    } 
+
+    /**
+     * Valida que la fecha nacimiento sea seleccionable y menor a la actual
+     */
+    public function testFechaMenorHoy()
+    {
+        $this->browse(function (Browser $browser){            
+            $browser->loginAs(User::find(1));//Superadmin
+            $browser->visit('/contactos/contactos/create');
+            $browser->assertDateTimeExistente('#fecha_nacimiento');
+            $browser->asignarFecha('#fecha_nacimiento', now());
+            $browser->press('Guardar');         
+            $browser->assertSee('debe ser antes de hoy');
+        }); 
+    } 
+
+    /**
+     * Valida que el prefijo se cargue de acuerdo con el género
+     */
+    public function testPrefijoConGenero()
+    {
+        $prefijo = Prefijo::find(1);
+        if(!empty($prefijo)){
+            $prefijo_erroneo = Prefijo::where('genero_id','<>',$prefijo->genero_id)->first();
+            $this->browse(function (Browser $browser) use ($prefijo,$prefijo_erroneo){            
+                $browser->loginAs(User::find(1));//Superadmin
+                $browser->visit('/contactos/contactos/create');
+                $browser->asignarValorSelect2('#genero_id',$prefijo->genero->nombre,$prefijo->genero->id);
+                $browser->assertValorEnSelect2('#prefijo_id',$prefijo->nombre,$prefijo_erroneo->nombre);
+            }); 
+        }        
+    } 
 }
