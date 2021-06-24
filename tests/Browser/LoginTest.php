4456d7993146c8f36c63844f2f2cfe4bd8f5e655
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-06-23 19:55:56 -0500

    Se añade test para usuario suspendido

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,16 +2,23 @@
 
 namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Admin\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 
 class LoginTest extends DuskTestCase
 {
-    use DatabaseTransactions;
 
+    //Para cerrar sesión después de cada método
+    protected function tearDown(): void
+    {
+        session()->flush();
+        parent::tearDown();
+    }
+
+    
     /**
-     * Valida que no permita ingresa hasta el home con autenticación incorrecta
+     * Valida que no permita ingresar con autenticación incorrecta
      */
     public function testAutenticacionInvalida()
     {
@@ -19,14 +26,36 @@ class LoginTest extends DuskTestCase
             $browser->visit('/login');
             $browser->type('email', 'superadmin@crm.com');
             $browser->type('password', 'contraseñaerrada');
-            $browser->press('Iniciar sesión');
-            $browser->assertPathIs('/login');
+            $browser->press('Iniciar sesión');            
+            $browser->assertSee('Usuario y/o contraseña incorrectos');
         });
 
     }
 
     /**
-     * Valida que permita ingresa hasta el home con autenticación válida
+     * Valida que no permita ingresar con un usuario suspendido
+     */
+    public function testUsuarioSuspendido()
+    {    
+        $usuario = User::where('email','auxiliar@crm.com')->first();
+        $usuario->active=0;
+        $usuario->save();    
+
+        $this->browse(function (Browser $browser) use ($usuario) {
+            $browser->visit('/login');
+            $browser->type('email', 'auxiliar@crm.com');
+            $browser->type('password', 'auxiliar');
+            $browser->press('Iniciar sesión');         
+            $browser->assertSee('Tu cuenta no está activa');
+
+            $usuario->active=1;
+            $usuario->save();
+        });
+
+    }
+
+    /**
+     * Valida que permita ingresar hasta el home con autenticación válida
      */
     public function testAutenticacionValida()
     {
