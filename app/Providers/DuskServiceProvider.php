865ee5983b6f2c07a4ec70449065c570445a25d6
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-06-27 18:43:51 -0500

    Actualización teniendo en cuenta que Dusk no se despliega en producción.

--- app/Providers/DuskServiceProvider.php
+++ app/Providers/DuskServiceProvider.php
@@ -16,125 +16,129 @@ class DuskServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Modifica el valor de un campo de tipo select2
-        \Laravel\Dusk\Browser::macro('asignarValorSelect2', function ($element,$class,$name,$id) {
-            //$this->waitFor('[aria-labelledby="select2-'.ltrim($element, '#').'-container"]'); 
-            if($class!="value"){
-                $objeto=$class::where('id',$id)->first(); 
-                $valor=$objeto->$name;
-            }else{               
-                $objeto=$class; 
-                $valor=$name;   
-            }
-            if(!empty($objeto) && !empty($valor) && !empty($id)){   
-                $script='jQuery("'.$element.'").append(
-                        $("<option selected=\'selected\'></option>")
-                        .val('.$id.').text("'.$valor.'")
-                    ).trigger("change");';
-                $this->script($script); 
-            }                     
-            return $this;
-        });
+        //Solo aplica en Local y Pruebas
+        if (in_array(env('APP_ENV'),['local','testing'])) {        
 
-        // Modifica el valor de un campo múltiple de tipo select2
-        \Laravel\Dusk\Browser::macro('asignarValorMultipleSelect2', function ($element,$class,$name,$ids) {
-            //$this->waitFor('.select2-selection--multiple'); 
-            if(sizeof($ids)>0){
-                $objetos=$class::whereIn('id',$ids)->get(); 
-                if($objetos->count()>0){
-                    foreach($objetos as $objeto){
-                        $this->script(
-                            'jQuery("'.$element.'").append(
-                                $("<option selected=\'selected\'></option>")
-                                .val('.$objeto->id.').text("'.$objeto->$name.'")
-                            ).trigger("change");');
-                    }  
-                }   
-            }              
-            return $this;
-        });
+            // Modifica el valor de un campo de tipo select2
+            \Laravel\Dusk\Browser::macro('asignarValorSelect2', function ($element,$class,$name,$id) {
+                //$this->waitFor('[aria-labelledby="select2-'.ltrim($element, '#').'-container"]'); 
+                if($class!="value"){
+                    $objeto=$class::where('id',$id)->first(); 
+                    $valor=$objeto->$name;
+                }else{               
+                    $objeto=$class; 
+                    $valor=$name;   
+                }
+                if(!empty($objeto) && !empty($valor) && !empty($id)){   
+                    $script='jQuery("'.$element.'").append(
+                            $("<option selected=\'selected\'></option>")
+                            .val('.$id.').text("'.$valor.'")
+                        ).trigger("change");';
+                    $this->script($script); 
+                }                     
+                return $this;
+            });
 
-        // Valida si existe un campo dentro de un seleccionable de tipo select2
-        \Laravel\Dusk\Browser::macro('assertValorEnSelect2', function ($element,$textoSi,$textoNo=null) {
-            $highlightedClass    = '.select2-results__option--highlighted';
-            $highlightedSelector = '.select2-results__options ' . $highlightedClass;
-            
-            $this->click($element.' + .select2');
-            $this->waitFor($highlightedSelector, 2);
-            $this->assertSee($textoSi);  
-            if(!empty($textoNo)){
-                $this->assertDontSee($textoNo);
-            }
-            $this->click($highlightedSelector);
-            return $this;
-        });
+            // Modifica el valor de un campo múltiple de tipo select2
+            \Laravel\Dusk\Browser::macro('asignarValorMultipleSelect2', function ($element,$class,$name,$ids) {
+                //$this->waitFor('.select2-selection--multiple'); 
+                if(sizeof($ids)>0){
+                    $objetos=$class::whereIn('id',$ids)->get(); 
+                    if($objetos->count()>0){
+                        foreach($objetos as $objeto){
+                            $this->script(
+                                'jQuery("'.$element.'").append(
+                                    $("<option selected=\'selected\'></option>")
+                                    .val('.$objeto->id.').text("'.$objeto->$name.'")
+                                ).trigger("change");');
+                        }  
+                    }   
+                }              
+                return $this;
+            });
 
-        /**
-         * Confirma que exista un elemento de tipo 
-         * datepicker al hacer clic sobre el elemento
-        **/
-        \Laravel\Dusk\Browser::macro('assertDateTimeExistente', function ($element) {
-            $this->click($element);
-            $claseDatePicker=".bootstrap-datetimepicker-widget";
-            $this->assertVisible($claseDatePicker);
-            return $this;
-        });
+            // Valida si existe un campo dentro de un seleccionable de tipo select2
+            \Laravel\Dusk\Browser::macro('assertValorEnSelect2', function ($element,$textoSi,$textoNo=null) {
+                $highlightedClass    = '.select2-results__option--highlighted';
+                $highlightedSelector = '.select2-results__options ' . $highlightedClass;
+                
+                $this->click($element.' + .select2');
+                $this->waitFor($highlightedSelector, 2);
+                $this->assertSee($textoSi);  
+                if(!empty($textoNo)){
+                    $this->assertDontSee($textoNo);
+                }
+                $this->click($highlightedSelector);
+                return $this;
+            });
 
-        //Asigna una fecha a un campo dateTimePicker
-        \Laravel\Dusk\Browser::macro('asignarFecha', function ($elemento,$fecha) {
-            $anio = $fecha->year;
-            $mes = $fecha->month;
-            $dia = $fecha->day;
-            $fecha_completa="{$anio}-{$mes}-{$dia}";
-            $script="document.querySelector('{$elemento}').value = '{$fecha_completa}'";
-            $this->script($script);
-            return $this;
-        });
+            /**
+             * Confirma que exista un elemento de tipo 
+             * datepicker al hacer clic sobre el elemento
+            **/
+            \Laravel\Dusk\Browser::macro('assertDateTimeExistente', function ($element) {
+                $this->click($element);
+                $claseDatePicker=".bootstrap-datetimepicker-widget";
+                $this->assertVisible($claseDatePicker);
+                return $this;
+            });
 
-         /**
-         * Valida que el componente de ubicación solicite el pais y en caso 
-         * de ser Colombia también debe pedir departamento y ciudad
-         */
-        \Laravel\Dusk\Browser::macro('assertValidarUbicacion', function ($elemento) {
-            $ubicacionExterior = Lugar::where('nombre','<>','Colombia')->where('tipo','P')->first();
-            $ubicacionColombia = Lugar::where('nombre','Colombia')->where('tipo','P')->first();
-            $ciudad = Lugar::where('tipo','C')->inRandomOrder()->first();
-            
-            if(!empty($ubicacionExterior) && !empty($ubicacionColombia) && !empty($ciudad)){
+            //Asigna una fecha a un campo dateTimePicker
+            \Laravel\Dusk\Browser::macro('asignarFecha', function ($elemento,$fecha) {
+                $anio = $fecha->year;
+                $mes = $fecha->month;
+                $dia = $fecha->day;
+                $fecha_completa="{$anio}-{$mes}-{$dia}";
+                $script="document.querySelector('{$elemento}').value = '{$fecha_completa}'";
+                $this->script($script);
+                return $this;
+            });
+
+            /**
+             * Valida que el componente de ubicación solicite el pais y en caso 
+             * de ser Colombia también debe pedir departamento y ciudad
+             */
+            \Laravel\Dusk\Browser::macro('assertValidarUbicacion', function ($elemento) {
+                $ubicacionExterior = Lugar::where('nombre','<>','Colombia')->where('tipo','P')->first();
+                $ubicacionColombia = Lugar::where('nombre','Colombia')->where('tipo','P')->first();
+                $ciudad = Lugar::where('tipo','C')->inRandomOrder()->first();
+                
+                if(!empty($ubicacionExterior) && !empty($ubicacionColombia) && !empty($ciudad)){
+                    $departamento = $ciudad->lugarPadre;
+                    $clase="App\Models\Parametros\Lugar";
+                    
+                    //En el caso de pais del exterior no debe solicitar nuevamente ubicación
+                    $this->asignarValorSelect2($elemento,$clase,'nombre',$ubicacionExterior->id);
+                    $this->assertNotPresent('.location-pre');
+                    $this->assertSeeIn($elemento.' + .select2', $ubicacionExterior->nombre);
+                    
+                    //En el caso de ser Colombia debe asignarla como enlace y pedir departamento
+                    $this->asignarValorSelect2($elemento,$clase,'nombre',$ubicacionColombia->id);
+                    $this->waitForLink($ubicacionColombia->nombre,2);
+                    $this->assertDontSeeIn($elemento.' + .select2', $ubicacionColombia->nombre);                
+                    
+                    //En caso de asignar departamento debe ser visible la ciudad y quedar establecida al seleccionar
+                    $this->asignarValorSelect2($elemento,$clase,'nombre',$departamento->id);
+                    $this->waitForLink($departamento->nombre,2);
+                    $this->asignarValorSelect2($elemento,$clase,'nombre',$ciudad->id);
+                    $this->assertSeeIn($elemento.' + .select2', $ciudad->nombre);
+                }
+                return $this;
+            });
+
+
+            \Laravel\Dusk\Browser::macro('asignarUbicacion', function ($elemento) {
+                $ubicacionColombia = Lugar::where('nombre','Colombia')->where('tipo','P')->first();
+                $ciudad = Lugar::where('tipo','C')->inRandomOrder()->first();
                 $departamento = $ciudad->lugarPadre;
                 $clase="App\Models\Parametros\Lugar";
-                 
-                //En el caso de pais del exterior no debe solicitar nuevamente ubicación
-                $this->asignarValorSelect2($elemento,$clase,'nombre',$ubicacionExterior->id);
-                $this->assertNotPresent('.location-pre');
-                $this->assertSeeIn($elemento.' + .select2', $ubicacionExterior->nombre);
-                
-                //En el caso de ser Colombia debe asignarla como enlace y pedir departamento
                 $this->asignarValorSelect2($elemento,$clase,'nombre',$ubicacionColombia->id);
-                $this->waitForLink($ubicacionColombia->nombre,2);
-                $this->assertDontSeeIn($elemento.' + .select2', $ubicacionColombia->nombre);                
-                
-                //En caso de asignar departamento debe ser visible la ciudad y quedar establecida al seleccionar
+                $this->waitForLink($ubicacionColombia->nombre,5);
                 $this->asignarValorSelect2($elemento,$clase,'nombre',$departamento->id);
-                $this->waitForLink($departamento->nombre,2);
+                $this->waitForLink($departamento->nombre,5);
                 $this->asignarValorSelect2($elemento,$clase,'nombre',$ciudad->id);
-                $this->assertSeeIn($elemento.' + .select2', $ciudad->nombre);
-            }
-            return $this;
-        });
-
-
-        \Laravel\Dusk\Browser::macro('asignarUbicacion', function ($elemento) {
-            $ubicacionColombia = Lugar::where('nombre','Colombia')->where('tipo','P')->first();
-            $ciudad = Lugar::where('tipo','C')->inRandomOrder()->first();
-            $departamento = $ciudad->lugarPadre;
-            $clase="App\Models\Parametros\Lugar";
-            $this->asignarValorSelect2($elemento,$clase,'nombre',$ubicacionColombia->id);
-            $this->waitForLink($ubicacionColombia->nombre,5);
-            $this->asignarValorSelect2($elemento,$clase,'nombre',$departamento->id);
-            $this->waitForLink($departamento->nombre,5);
-            $this->asignarValorSelect2($elemento,$clase,'nombre',$ciudad->id);
-            return $this;
-        });
+                return $this;
+            });
+        }
     }
 }
