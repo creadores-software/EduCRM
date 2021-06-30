4c2650a3462922c65073bb009ec2d43569f77788
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-06-29 23:41:16 -0500

    Se actualiza para ejecutar migraciones al incio de los test de Ducker

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -6,6 +6,9 @@ use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Laravel\Dusk\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Contracts\Console\Kernel;
+use Mockery;
 
 abstract class DuskTestCase extends BaseTestCase
 {
@@ -58,5 +61,55 @@ abstract class DuskTestCase extends BaseTestCase
     {
         return isset($_SERVER['DUSK_HEADLESS_DISABLED']) ||
                isset($_ENV['DUSK_HEADLESS_DISABLED']);
+    }
+
+    public function createApplication()
+    {
+        return self::initialize();
+    }
+
+    private static $configurationApp = null;
+
+    public static function initialize(){
+
+        if(is_null(self::$configurationApp)){
+            $app = require __DIR__.'/../bootstrap/app.php';
+            $app->loadEnvironmentFrom('.env.testing');
+            $app->make(Kernel::class)->bootstrap();
+
+            if (config('database.default') == 'sqlite') {
+                $db = app()->make('db');
+                $db->connection()->getPdo()->exec("pragma foreign_keys=1");
+            }
+
+            Artisan::call('migrate');
+            Artisan::call('db:seed');
+
+            self::$configurationApp = $app;
+            return $app;
+        }
+
+        return self::$configurationApp;
+    }
+
+    protected function tearDown(): void
+    {
+        if ($this->app) {
+            foreach ($this->beforeApplicationDestroyedCallbacks as $callback) {
+                call_user_func($callback);
+            }
+        }
+
+        $this->setUpHasRun = false;
+
+        if (property_exists($this, 'serverVariables')) {
+            $this->serverVariables = [];
+        }
+
+        if (class_exists('Mockery')) {
+            Mockery::close();
+        }
+        $this->afterApplicationCreatedCallbacks = [];
+        $this->beforeApplicationDestroyedCallbacks = [];
     }
 }
