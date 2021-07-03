074209eb468964bc1c1cc0c6a824aa6d4b88481a
Valentina Londoño Marin <valoma.vld@gmail.com>
2021-07-03 00:20:04 -0500

    Se ajusta validación según cada caso

--- resources/views/contactos/segmentos/filtros.blade.php
+++ resources/views/contactos/segmentos/filtros.blade.php
@@ -102,24 +102,25 @@
             if(idSegmento!=null){                    
                 actualizarSegmento(idSegmento);    
                 filtrarDataTable(); 
-            }
-            /**
-            Se pone de nuevo en document ready para que se ejecute después
-            que estén instanciados los select2
-            */
-            jQuery(document).ready(function(){                
-                var filtros_texto = sessionStorage.getItem('filtros_texto'); 
-                    if(filtros_texto!=null){
-                        var filtros = filtros_texto.split(';'); 
-                        $.each(filtros, function(index, filtro) {                
-                            if(filtro){
-                                var valores = filtro.split(':');
-                                actualizarCampo(valores[0], valores[1]); 
-                            }                          
-                        });
-                    }   
-                filtrarDataTable();   
-            });           
+            }else{
+                /**
+                Se pone de nuevo en document ready para que se ejecute después
+                que estén instanciados los select2
+                */
+                jQuery(document).ready(function(){                
+                    var filtros_texto = sessionStorage.getItem('filtros_texto'); 
+                        if(filtros_texto!=null){
+                            var filtros = filtros_texto.split(';'); 
+                            $.each(filtros, function(index, filtro) {                
+                                if(filtro){
+                                    var valores = filtro.split(':');
+                                    actualizarCampo(valores[0], valores[1]); 
+                                }                          
+                            });
+                        }   
+                    filtrarDataTable();   
+                });
+            }         
         }
 
         function actualizarCampo(campo, valor){
